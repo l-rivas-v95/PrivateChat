1c2db8cb51fc0fbd77e4475b45529088362356a2
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-12 20:22:18 +0200

    Add chat list timestamp field

--- PrivateChat/app/src/main/java/com/lrv/privatechat/model/ChatItemUiModel.kt
+++ PrivateChat/app/src/main/java/com/lrv/privatechat/model/ChatItemUiModel.kt
@@ -3,5 +3,6 @@ package com.lrv.privatechat.model
 data class ChatItemUiModel(
     val username: String,
     val displayName: String,
-    val lastMessage: String
+    val lastMessage: String,
+    val timeText: String
 )
