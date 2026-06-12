4b40091145a54e8b0319c31c6d21296be74c37f9
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-12 20:42:54 +0200

    Add avatar to chat list model

--- PrivateChat/app/src/main/java/com/lrv/privatechat/model/ChatItemUiModel.kt
+++ PrivateChat/app/src/main/java/com/lrv/privatechat/model/ChatItemUiModel.kt
@@ -4,5 +4,6 @@ data class ChatItemUiModel(
     val username: String,
     val displayName: String,
     val lastMessage: String,
-    val timeText: String
+    val timeText: String,
+    val avatarBase64: String? = null
 )
