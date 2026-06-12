b1ce1f50c98ecddc568fc2d614aead81705d9c04
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-12 15:55:11 +0200

    Add contact DAO

---
+++ PrivateChat/app/src/main/java/com/lrv/privatechat/data/dao/ContactDao.kt
@@ -0,0 +1,18 @@
+package com.lrv.privatechat.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.lrv.privatechat.data.entity.ContactEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ContactDao {
+
+    @Query("SELECT * FROM contacts ORDER BY displayName ASC")
+    fun observeContacts(): Flow<List<ContactEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(contact: ContactEntity)
+}
