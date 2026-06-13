6816fc2007b7fd723ce8a422fc53c58deb2e7813
Luis Rivas Villa <luisrivi95@gmail.com>
2026-06-13 17:57:35 +0200

    merge2

--- PrivateChat/app/src/main/java/com/lrv/privatechat/network/ChatWebSocketClient.kt
+++ PrivateChat/app/src/main/java/com/lrv/privatechat/network/ChatWebSocketClient.kt
@@ -6,7 +6,6 @@ import okhttp3.Request
 import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
-import com.lrv.privatechat.BuildConfig
 
 class ChatWebSocketClient(
     private val onMessageReceived: (String) -> Unit,
