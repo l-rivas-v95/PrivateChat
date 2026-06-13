d7350361d778e065a0f85329da9ba2ee3f9b626e
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-13 17:06:36 +0200

    Add viewmodel compose dependency

--- PrivateChat/app/build.gradle.kts
+++ PrivateChat/app/build.gradle.kts
@@ -59,6 +59,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.room.runtime)
